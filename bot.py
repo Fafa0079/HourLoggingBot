49db0a8a753c723bc92d61826afa690420cd2dcd
Ricky Ward <rward0079@outlook.com>
2024-02-05 08:56:35 -0500

    Automatic Sync Sucks (removed autosync, added manual sync)

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 
 from datetime import datetime
-from typing import List
+from typing import List, Literal, Optional
 
 import discord
 from discord import Embed, app_commands
@@ -48,6 +48,40 @@ async def log_autocomplete(
         app_commands.Choice(name = subteam, value = subteam)
         for subteam in SUBTEAMS if current.lower() in subteam.lower()
     ]
+
+@bot.tree.command()
+@commands.guild_only()
+@commands.is_owner()
+async def sync(ctx: commands.Context, guilds: commands.Greedy[discord.Object], spec: Optional[Literal["~", "*", "^"]] = None) -> None:
+    if not guilds:
+        if spec == "~":
+            synced = await ctx.bot.tree.sync(guild=ctx.guild)
+        elif spec == "*":
+            ctx.bot.tree.copy_global_to(guild=ctx.guild)
+            synced = await ctx.bot.tree.sync(guild=ctx.guild)
+        elif spec == "^":
+            ctx.bot.tree.clear_commands(guild=ctx.guild)
+            await ctx.bot.tree.sync(guild=ctx.guild)
+            synced = []
+        else:
+            synced = await ctx.bot.tree.sync()
+
+        await ctx.send(
+            f"Synced {len(synced)} commands {'globally' if spec is None else 'to the current guild.'}"
+        )
+        return
+
+    ret = 0
+    for guild in guilds:
+        try:
+            await ctx.bot.tree.sync(guild=guild)
+        except discord.HTTPException:
+            pass
+        else:
+            ret += 1
+
+    await ctx.send(f"Synced the tree to {ret}/{len(guilds)}.")
+    # | no params sync all | ~ syncs guild | * syncs global commands to guild | ^ removes all commands from guild |
 
 
 bot.run(BOT_TOKEN)
