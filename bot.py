203a2f097124d5632ee35902f7952cb78c1750d5
Ricky Ward <rward0079@outlook.com>
2024-02-22 17:38:14 -0500

    basic setup of the setup command (lol) + various bugfixes and changes throughout the code

--- bot.py
+++ bot.py
@@ -49,10 +49,44 @@ async def sync(ctx: commands.Context, guilds: commands.Greedy[discord.Object], s
 
     await ctx.send(f"Synced the tree to {ret}/{len(guilds)}.")
     # | no params syncs all | ~ syncs guild | * syncs global commands to guild | ^ removes all commands from guild |
+class SetupPages(View):
+    def __init__(self, pages):
+        super().__init__()
+        self.page = 0
+        self.pages = pages
+
+    @discord.ui.button(label="<", style = ButtonStyle.primary, custom_id="prev")
+    async def prev(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if self.page > 0:
+            self.page -= 1
+            await interaction.response.edit_message(embed=self.pages[self.page])
+            
+    @discord.ui.button(label="bruh", style=ButtonStyle.secondary, custom_id="pageinfo", disabled=True)
+    async def pageinfo(self, interaction: discord.Interaction, button: discord.ui.Button):
+        button.label = f"Step {self.page + 1}/{len(self.pages)}"
+
+    @discord.ui.button(label=">", style=ButtonStyle.primary, custom_id="next")
+    async def next(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if self.page < len(self.pages) - 1:
+            self.page += 1
+            await interaction.response.edit_message(embed=self.pages[self.page])
 
 @bot.tree.command(name="setup", description="Guided setup for hour logging.")
-async def config(interaction: discord.Interaction):
-    print("placeholder")
+@commands.has_permissions(administrator=True)
+async def setup(interaction: discord.Interaction):
+    p1Embed = discord.Embed(color=discord.Color.blue(), title="Hour Logging Setup", description=f"Welcome to the hour logging setup {interaction.user.display_name}! This guided setup will walk you through multiple steps to setup hour logging in your discord server. Listed here are the steps:\n1. Allow integration of discord bot w/ google sheets\n2. Add all subteams/groups\n3. Selection of admin channel to send hour logging approval messages\n4. Toggle use of /verify command")
+    p2Embed = discord.Embed(color=discord.Color.blue(), title="Google Sheets Integration", description="placeholder")
+    p3Embed = discord.Embed(color=discord.Color.blue(), title="Add subteams/groups", description="placeholder")
+    p4Embed = discord.Embed(color=discord.Color.blue(), title="Admin Channel Selection", description="placeholder")
+    p5Embed = discord.Embed(color=discord.Color.blue(), title= "Verify Command Usage", description="placeholder")
+    pages = [p1Embed, p2Embed, p3Embed, p4Embed, p5Embed]
+    try:
+        await interaction.response.send_message(embed=p1Embed, view = SetupPages(pages), ephemeral=True)
+    except Exception as e:
+        rEmbed = discord.Embed(color=discord.Color.red(), title="Setup Failed", description=f"Setup command failed due to an unexpected error.\n\nError:\n\n{e}")
+        await interaction.response.send_message(embed=rEmbed)
+        traceback.print_exception(type(e), e, e.__traceback__)
+        
 
 class EditHours(discord.ui.Modal, title="Edit Hours"):
     time = discord.ui.TextInput(
@@ -60,12 +94,19 @@ class EditHours(discord.ui.Modal, title="Edit Hours"):
         placeholder="Enter new number of hours here...")
     
     async def on_submit(self, interaction: discord.Interaction):
-        # send google api request to add hours to spreadsheet
-        sEmbed = discord.Embed(color = discord.Color.green(), title="Edit Hours", description=f"Sucessfully changed time to **{self.time.value}** hours and submitted hour request.")
-        await interaction.response.edit_message(embed=sEmbed, view=None, delete_after=30)
+        try:
+            float(self.time.value)
+        except TypeError as e:
+            rEmbed = discord.Embed(color=discord.Color.red(), title="Edit Hours", description=f"Error: Failed to edit hours because {self.time.value} is not a number. Please input a valid number.\n\nFull error:{e}")
+            await interaction.response.edit_message(embed=rEmbed)
+            print("Likely user error above, ignore ^^^^^^^^^^^^^^^^^^")
+        else:
+            # send google api request to add hours to spreadsheet
+            sEmbed = discord.Embed(color = discord.Color.green(), title="Edit Hours", description=f"Sucessfully changed time to **{self.time.value}** hours and submitted hour request.")
+            await interaction.response.edit_message(embed=sEmbed, view=None, delete_after=30)
 
     async def on_error(self, interaction: discord.Interaction, error: Exception) -> None: 
-        rEmbed = discord.Embed(color = discord.Color.red(), title="Edit Hours", description=f"Failed to change hours because of an error.\n\nError(s):\n\n{error}")
+        rEmbed = discord.Embed(color = discord.Color.red(), title="Edit Hours", description=f"Failed to change hours due to an unexpected error.\n\nError(s):\n\n{error}")
         await interaction.response.edit_message(embed=rEmbed, view=None)
         traceback.print_exception(type(error), error, error.__traceback__)
 
@@ -86,7 +127,7 @@ class DenyHours(discord.ui.Modal, title='Deny Hours'):
         await interaction.response.edit_message(embed=sEmbed, view=None, delete_after=30)
 
     async def on_error(self, interaction: discord.Interaction, error: Exception) -> None: 
-        rEmbed = discord.Embed(color = discord.Color.red(), title="Edit Hours", description=f"Failed to decline hours due of an error.\n\nError(s):\n\n{error}")
+        rEmbed = discord.Embed(color = discord.Color.red(), title="Edit Hours", description=f"Failed to decline hours due to an unexpected error.\n\nError(s):\n\n{error}")
         await interaction.response.edit_message(embed=rEmbed, view=None)
         traceback.print_exception(type(error), error, error.__traceback__)
 
@@ -104,7 +145,7 @@ class VerifyHours(View):
             await interaction.response.edit_message(embed=cEmbed, view=None, delete_after=30)
         except Exception as e:
             traceback.print_exception(type(e), e, e.__traceback__)
-            rEmbed = discord.Embed(color = discord.Color.red(), title="Edit Hours", description=f"Failed to confirm hours due of an error.\n\nError(s):\n\n{e}")
+            rEmbed = discord.Embed(color = discord.Color.red(), title="Edit Hours", description=f"Failed to confirm hours due to an unexpected error.\n\nError(s):\n\n{e}")
             await interaction.response.edit_message(embed=rEmbed, view=None)
     @discord.ui.button(label="Edit", style=ButtonStyle.secondary, custom_id="edit", emoji="✏")
     async def edit(self, interaction: discord.Interaction, button: discord.ui.Button):
@@ -117,7 +158,7 @@ class VerifyHours(View):
 
 @bot.tree.command(name="log", description= "Command used for logging hours.")
 @app_commands.describe(subteam= "Your subteam", time= "Hours spent", description= "Explanation of tasks")
-async def log(interaction: discord.Interaction, subteam: str, time: int, description: str):
+async def log(interaction: discord.Interaction, subteam: str, time: float, description: str):
         if (SUBTEAMS.count(subteam) != 0):
             try: # attempt to send message to admin channel
                 user = interaction.user
@@ -125,13 +166,13 @@ async def log(interaction: discord.Interaction, subteam: str, time: int, descrip
                 await discord.Guild.get_channel(interaction.guild, ADMIN_CHANNEL).send(embed = rEmbed, view=VerifyHours(time, user, subteam))
             except Exception as e: # if fails, print error
                 print(e)
-                eEmbed = discord.Embed(color = discord.Color.red(), title = "Hour Logging", description =f"Failed to send hour request due to an error.\n\n**Error(s):**\n\n{e}")
+                eEmbed = discord.Embed(color = discord.Color.red(), title = "Hour Logging", description =f"Failed to send hour request due to an unexpected error.\n\n**Error(s):**\n\n{e}")
                 await interaction.response.send_message(embed = eEmbed, ephemeral = True)
             else: # if succeeds, gives success message
                 sEmbed = discord.Embed(color = discord.Color.green(), title = "Hour Logging", description =f"Successfully sent hour request!\n\n**User**\n{interaction.user.display_name}\n\n**Subteam**\n{subteam}\n\n**Time spent**\n{time} hour(s)\n\n**Tasks**\n{description}")
                 await interaction.response.send_message(embed = sEmbed, ephemeral = True)
         else: # if subteam is invalid
-            uEmbed = discord.Embed(color = discord.Color.red(), title = "Hour Logging", description =f"Failed to send hour request because '{subteam}' is not a valid subteam.")
+            uEmbed = discord.Embed(color = discord.Color.red(), title = "Hour Logging", description =f"Failed to send hour request because '{subteam}' is not a valid subteam. Please input a valid subteam.")
             await interaction.response.send_message(embed = uEmbed, ephemeral = True) 
 
 @log.autocomplete("subteam")
